98ddfa8f36c8ada318facc36e562f594f0982e89
Madeleine Gómez Moreira <claragomezmoreira@gmail.com>
2026-05-11 18:02:46 +0200

    Chore: implement csv, json and xml file readers

--- src/main/java/com/springbatchpractice/writer/JsonItemWriter.java
+++ src/main/java/com/springbatchpractice/writer/JsonItemWriter.java
@@ -1,16 +1,17 @@
 package com.springbatchpractice.writer;
 
 import com.springbatchpractice.model.StudentCsv;
+import com.springbatchpractice.model.StudentJson;
 import org.springframework.batch.infrastructure.item.Chunk;
 import org.springframework.batch.infrastructure.item.ItemWriter;
 import org.springframework.stereotype.Component;
 
 @Component
-public class FirstItemWriter implements ItemWriter<StudentCsv> {
+public class JsonItemWriter implements ItemWriter<StudentJson> {
 
     //Chunk contains a list of items. The size of the list in the method arguments depends on the chunk size
     @Override
-    public void write(Chunk<? extends StudentCsv> chunk) {
+    public void write(Chunk<? extends StudentJson> chunk) {
         System.out.println("Inside Item Writer");
         chunk.getItems().forEach(System.out::println);
     }
--- src/main/java/com/springbatchpractice/writer/XmlItemWriter.java
+++ src/main/java/com/springbatchpractice/writer/XmlItemWriter.java
@@ -1,16 +1,17 @@
 package com.springbatchpractice.writer;
 
-import com.springbatchpractice.model.StudentCsv;
+import com.springbatchpractice.model.StudentJson;
+import com.springbatchpractice.model.StudentXml;
 import org.springframework.batch.infrastructure.item.Chunk;
 import org.springframework.batch.infrastructure.item.ItemWriter;
 import org.springframework.stereotype.Component;
 
 @Component
-public class FirstItemWriter implements ItemWriter<StudentCsv> {
+public class XmlItemWriter implements ItemWriter<StudentXml> {
 
     //Chunk contains a list of items. The size of the list in the method arguments depends on the chunk size
     @Override
-    public void write(Chunk<? extends StudentCsv> chunk) {
+    public void write(Chunk<? extends StudentXml> chunk) {
         System.out.println("Inside Item Writer");
         chunk.getItems().forEach(System.out::println);
     }
